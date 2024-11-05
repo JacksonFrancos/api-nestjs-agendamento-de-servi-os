a7099f6d9cebe913bd1884e285edf8eb25ab59a3
Jackson <jackssfranco@hotmail.com>
2024-11-04 21:35:12 -0300

    estabelecendo conexão com o banco de dados

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,20 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ClientsModule } from './clients/clients.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ClientsModule],
+  imports: [ClientsModule, TypeOrmModule.forRoot({
+    type : 'postgres',
+    host : 'localhost',
+    port: 5432,
+    username: 'postgres',
+    password: 'jack',
+    database: 'test',
+    entities: [],
+    synchronize: true,
+  })],
+
   controllers: [AppController],
   providers: [AppService],
 })
--- src/clients/entities/client.entity.ts
+++ src/clients/entities/client.entity.ts
@@ -1,1 +1,17 @@
-export class Client {}
+import { UUID } from 'crypto';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Client {
+  @PrimaryGeneratedColumn()
+  id: UUID;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  phone: number;
+}
